4371b4e774845e413ca3368a3bd0ae359f1bbeb0
Arnold <arnold.pham@hotmail.com>
2022-06-17 08:08:24 +0200

    BDD pour insertion rapide
    BDD avec données de 5 personnes fictives

---
+++ --SQLite.sql
@@ -0,0 +1,7 @@
+--SQLite
+INSERT INTO employes (prenom, nom, telephone, email, adresse, poste, salaire, datedenaissance) VALUES
+("Camille", "Baubier", 0678230321, "camille.baubier@gmail.com", "10 rue de rivoli 75001 Paris", "Développeur Web", 2500, "1982-05-01"),
+("Thomas", "Winter", 0689773290, "thomwinter@hotmail.fr", "15 avenue jean jaures 33000 Bordeaux", "Développeur Web", 2800, "1979-12-26"),
+("Julien", "Cottet", 0626662521, "cottet.julien@gmail.com", "70 rue royale 06000 Nice", "Web Designer", 1900, "1991-02-27"),
+("Mathieu", "Valse", 0658125001, "mathieuv@yahoo.fr", "87 avenue du printemps 69000 Lyon", "Stagiaire", 1500, "1992-08-20"),
+("Amandine", "Thoyer", 0622990010, "amandine.thoyer@gmail.com", "15 rue du vieu port 13000 Marseille", "Directeur", 3100, "1985-09-09");
